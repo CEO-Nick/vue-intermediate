bbf2ce60065c4288f4b4897809309826a6494356
nick <andantej281@gmail.com>
2025-09-11 22:30:25 +0900

    섹션 3. TodoInput 컴포넌트 개발

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,63 @@
 <template>
-    <div>
-        input
+    <div class="inputBox shadow">
+        <!-- v-model: input에 입력된 값을 동적으로 vue 인스턴스 안에 매핑하는 역할 -->
+         <!-- 화면에서의 데이터 변경과 Vue 인스턴스의 데이터 변경이 서로 동기화되어 있다 -->
+          <!-- keyup.enter : input 박스에서 입력 후 엔터 눌렀을 때 동작할 메서드 -->
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
 <script>
     export default {
+        data: function() {
+            return {
+                newTodoItem: ""     // input에 입력되는 텍스트 값을 저장할 변수
+            }
+        },
+        methods: {
+            addTodo: function() {
+                // 여기서 this는 TodoInput component를 가리킴
+                localStorage.setItem(this.newTodoItem, this.newTodoItem);
+                this.clearInput();
+            },
+            clearInput: function() {
+                this.newTodoItem = '';
+            }
+        }
         
     }
 </script>
 
-<style lang="scss" scoped>
 
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
-        todo list
+        <ul>
+            <!-- todoItems에 있는 만큼 뿌려줌 -->
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+                {{ todoItem }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function() {
+            return {
+                todoItems: []
+            }
+        },
+
+        // 인스턴스가 생성되지마자 호출되는 라이프 사이클 훅
+        created: function() {
+            console.log('created');
+            if (localStorage.length <= 0) return;
+            for (var i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) === 'naveruserlocale') continue;
+                this.todoItems.push(localStorage.key(i))
+            }
+
+        }
     }
 </script>
 
